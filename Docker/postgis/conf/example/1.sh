44bff1e0ef9dcb846cb504aea0e688e35ce7959e
Wouter Scherphof <wouter.scherphof@gmail.com>
2019-02-12 12:40:59 +0100

    updates from upstream
    new/updated templates in docker4gis

--- Docker/postgis/conf/example/1.sh
+++ Docker/postgis/conf/example/1.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-pg.sh -c "
-    create schema example
+pg.sh -c '
+    create schema $(dirname "$0")
     ;
-"
+'
 
-mkdir ./1/shp
+mkdir -p ./1/shp
 unzip ./1/*-shp.zip -d ./1/shp
 shp2pgsql -D -I ./1/shp/*.shp example.firestation > ./1/load.sql
 pg.sh -f ./1/load.sql
+rm -rf ./1/shp
