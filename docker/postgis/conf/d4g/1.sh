378be890d18ca66bcba5ff417f63d79379d9b638
Wouter Scherphof <wouter.scherphof@gmail.com>
2019-02-14 17:36:10 +0100

    var size inhabitants/station

--- docker/postgis/conf/d4g/1.sh
+++ docker/postgis/conf/d4g/1.sh
@@ -11,8 +11,15 @@ pushd 1
 mkdir -p shp
 unzip *-shp.zip -d shp
 pushd shp
-shp2pgsql -D -I *.shp "${schema}.firestation" > load.sql
+shp2pgsql -D -I *.shp d4g.tbl_firestation > load.sql
 pg.sh -f load.sql
 popd
 rm -rf shp
 popd
+
+pg.sh -c "
+    create view d4g.vw_firestation as
+    select gid as fid, geom
+    from d4g.tbl_firestation
+    ;
+"
--- docker/postgis/conf/d4g/2.sh
+++ docker/postgis/conf/d4g/2.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
 pg.sh -c "
-    create table d4g.municipality (
+    create table d4g.tbl_municipality (
         municipality text,
         inhabitants int
     );
-    copy d4g.municipality
+    copy d4g.tbl_municipality
         from '$(pwd)/2/municipality.csv'
         with (format csv)
+    ;
+"
+
+pg.sh -c "
+    drop view d4g.vw_firestation
+    ;
+    create view d4g.vw_firestation as
+    with num as (
+        select count(*) as num, gemeente
+        from d4g.tbl_firestation
+        group by gemeente
+    ), ratio as (
+        select inhabitants / num as ratio, municipality
+        from num, d4g.tbl_municipality
+        where gemeente = municipality
+    )
+    select gid as fid, geom, (ratio / 1000)::int as size
+    from ratio, d4g.tbl_firestation
+    where gemeente = municipality
     ;
 "
